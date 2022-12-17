58dc414891f4f3a5db6518efccc750adabdf4ec6
Rohan B <rohbhot@users.noreply.github.com>
2022-12-17 10:58:54 +0530

    Update Mod_TCP_Client.py

--- Mod_TCP_Client.py
+++ Mod_TCP_Client.py
@@ -1,13 +1,10 @@
-import pymodbus                                   # Install pymodbus library using command pip install pymodbus
-from pymodbus.pdu import ModbusRequest
 from pymodbus.client.sync import ModbusTcpClient
 
 client = ModbusTcpClient('192.168.0.100')         # IP address of the server / ABB PLC in this case
-print client
 conn = client.connect()
-print conn
 
-request = client.read_holding_registers(11,10)    # Reads the value of 10 holding registers starting form the memory adress directly mapped to 40,000
-print request.registers
+if conn:
+  request = client.read_holding_registers(11,10)    # Reads the value of 10 holding registers starting form the memory adress directly mapped to 40,000
+  print request.registers
 
-client.write_register(10,200)                     # Write the value 200 to the holding register 10 or 40,010. 
+  client.write_register(10,200)                     # Write the value 200 to the holding register 10 or 40,010. 
